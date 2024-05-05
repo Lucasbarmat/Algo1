9ec08312c69396f1ac0bf79dbf3a4bd24f3359a2
Lucasbarmat <lucasbarmat@gmail.com>
2024-05-05 20:38:32 -0300

    g

--- parciales/simu.hs
+++ parciales/simu.hs
@@ -1,18 +1,6 @@
 --Ejercicio 1
-    --Para empezar a diseñar la novedosa y rupturista red social Y el famoso Elio Mark nos ha pedido que desarrollemos algunas funciones básicas, 
-    --que tendrán como objetido representar algunas relaciones e interacciones entre los usuarios. Para esto nos envió las siguientes especificaciones en lenguaje semiformal y 
-    --nos pidió que hagamos el desarrollo enteramente en Haskell, utilizando los tipos requeridos 
-    --y solamente las funciones que se ven en Introducción a la Programación de Exactas-UBA.
- 
-    --problema relacionesValidas (relaciones: seq⟨String x String⟩) : Bool {
-      --requiere: {True}
-      --asegura: {(res = true) <=> relaciones no contiene ni tuplas repetidas1, ni tuplas con ambas componentes iguales}
-    --}
-    --1 A los fines de este problema consideraremos que dos tuplas son iguales si el par de elementos que las componen (sin importar el orden) son iguales.
- 
 -- NO SE REPITAN LAS TUPLAS
 -- NO HAY TUPLAS CON LOS MISMOS COMPEONENETES EN OTRO ORDEN
-
 relacionesValidas :: [(String, String)] ->Bool
 relacionesValidas [] = True
 relacionesValidas ((a,b):x) = (a /= b) && not(estaEnLaLista (a,b) x) && (relacionesValidas x) 
@@ -21,20 +9,30 @@ estaEnLaLista :: (String, String) ->[(String, String)] ->Bool
 estaEnLaLista _ [] = False
 estaEnLaLista (a,b) (x:xs) = ((a,b) == x) ||  ((b,a) == x) || (estaEnLaLista (a,b) xs)
 
+--otra forma de hacerlo
+relacionesValidas1 :: [(String, String)] ->Bool
+relacionesValidas1 [] = True
+relacionesValidas1 ((a,b):x) = (a /= b) && (noEstaEnLaLIsta(a,b) x) && relacionesValidas1 x 
+
+noEstaEnLaLIsta :: (String, String) ->[(String, String)] ->Bool
+noEstaEnLaLIsta _ [] = True
+noEstaEnLaLIsta (a,b) ((a1,b1):x) = (a /= a1) && (a /= b1) && (b /= a1) && (b /= b1) && (noEstaEnLaLIsta (a1, b1) x)
 
 
+
+
+--Ejercicio 2
     --problema personas (relaciones: seq⟨String x String⟩) : seq⟨String⟩ {
       --requiere: {relacionesValidas(relaciones)}
       --asegura: {res no tiene elementos repetidos}
       --asegura: {res tiene exactamente los elementos que figuran en alguna tupla de relaciones, en cualquiera de sus posiciones}
     --}
 
-
-personas :: [(String,String)] -> [String]
+personas :: [(String, String)] -> [String]
 personas [] = []
 personas x = eliminarRepetidoss (juntarlista(x))
 
-juntarlista :: [(String,String)] -> [String]
+juntarlista :: [(String, String)] -> [String]
 juntarlista [] = []
 juntarlista ((a,b):x) = [a] ++ [b] ++ juntarlista x
 
@@ -48,25 +46,29 @@ pertenecee _ [] = False
 pertenecee a (x:xs) | a == x = True
                     | otherwise = pertenecee a xs
 
+
+
+
+--Ejercicio 3
     --problema amigosDe (persona: String, relaciones: seq⟨String x String⟩) : seq⟨String⟩ {
       --requiere: {relacionesValidas(relaciones)}
       --asegura: {res tiene exactamente los elementos que figuran en las tuplas de relaciones en las que una de sus componentes es persona}
     --}
-
 amigosDe :: String -> [(String,String)] -> [String]
 amigosDe _ [] = []
 amigosDe persona ((a,b):x)  | persona == a = [b] ++ amigosDe persona x
                             | persona == b = [a] ++ amigosDe persona x
                             | otherwise = amigosDe persona x
 
- 
+
+
+
+ --Ejercicio 4
     --problema personaConMasAmigos (relaciones: seq⟨String x String⟩) : String {
       --requiere: {relaciones no vacía}
       --requiere: {relacionesValidas(relaciones)}
       --asegura: {res es el Strings que aparece más veces en las tuplas de relaciones (o alguno de ellos si hay empate)}
     --}
-
-
 personaConMasAmigos :: [(String, String)]  -> String
 personaConMasAmigos [] = " "
 personaConMasAmigos ((a,b):x) = comparar (juntarlista(((a,b):x)))
