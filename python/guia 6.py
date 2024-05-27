8bd2b21a5687c463bddfd7eba7e7da1cfcfc15ce
Lucasbarmat <lucasbarmat@gmail.com>
2024-05-27 13:00:32 -0300

    guia6

--- python/guia 6.py
+++ python/guia 6.py
@@ -160,10 +160,16 @@ def vacaciones_o_trabajar(sexo:str,edad:int):
     elif sexo == "F" and edad >=60: return "Andá de vacaciones"
     else: return "Te toca trabajar"
 
-print(vacaciones_o_trabajar("M",60))
+#print(vacaciones_o_trabajar("M",60))
 
 #----------------------------------------------------
 #6.1)
+def numero_del_1_al_10():
+    x:int = 1
+    while x<= 10:
+        print(x)
+        x += 1
+#numero_del_1_al_10()
 
 #6.2)
 def numerosParesEntre10y40 (): 
@@ -171,36 +177,139 @@ def numerosParesEntre10y40 ():
     while x<=40: 
         if x%2 == 0: 
             print (x) 
-            x += 1 
-#print(numerosParesEntre10y40 ()) 
+        x += 1 
+#numerosParesEntre10y40 ()
 
 #6.3)
+def eco_10_veces():
+    x:int = 1
+    while x<=10:
+        print("eco")
+        x += 1
+#eco_10_veces()
 
 #6.4)
 def cuentaRegre (numero:int): 
     while numero >0: 
         print(numero) 
-    numero -= 1 
+        numero -= 1 
     print("despegue") 
-    cuentaRegre (5)
+#cuentaRegre (5)
 
 #6.5)
+def viaje_en_el_tiempo(año_partida:int,año_llegada:int):
+    while año_partida > año_llegada:
+        año_partida -= 1
+        print(f"Viajo un año al pasado, estamos en el año: {año_partida}")
+#viaje_en_el_tiempo(2010,2000)
+
 #6.6)
+def viaje_rapido(salida:int):
+    while salida>= -384:
+        salida -= 20
+    return print("llegamos")
+#viaje_rapido(1900)
 
 #----------------------------------------------------
-#7)
+#7.1)
+def AUXnumero_del_1_al_10():
+    for num in range(1,11,1):
+        print(num)
+#AUXnumero_del_1_al_10()
 
+#7.2)
+def AUXnumerosParesEntre10y40 (): 
+    for num in range(10,41,2):
+        print(num)
+#AUXnumerosParesEntre10y40()
+
+#7.3)
+def AUXeco_10_veces():
+    for num in range(1,10,1):
+        print("eco")
+#AUXeco_10_veces()
+
+#7.4)
+def AUXcuentaRegre (numero:int):
+    for num in range(numero,0,-1):
+        print(num) 
+    print("despegue")
+#AUXcuentaRegre(4)
+
+#7.5)
+def AUXviaje_en_el_tiempo(año_partida:int,año_llegada:int):
+    for año in range(año_partida-1,año_llegada-1,-1):
+        print(f"Viajó un año al pasado, estamos en el año: {año}")
+#AUXviaje_en_el_tiempo(2010,2005)
+
+#7.6)
+def AUXviaje_rapido(salida:int):
+    for año in range(salida,-384,-20):
+        print(f"Viajó 20 años al pasado, estamos en el año: {año}")
+#AUXviaje_rapido(133)
+   
 #----------------------------------------------------
 #8.1)
+def uno():
+    x = 5
+    y = 7
+    x = x + y
+    return print(x)
+#uno()
+
 #8.2)
+def dos():
+    x=5
+    y=7
+    z=x+y
+    y = z * 2
+    return y
+#print(dos())
+
 #8.3)
+def tres():
+    x=5 
+    y=7
+    x="hora"
+    y = x * 2
+    return y
+#print(tres())
+
 #8.4)
+def cuatro():
+    x=False
+    return not(x)
+#print(cuatro())
+
 #8.5)
+def cinco():
+    x=False
+    x=not(x)
+    return x
+#print(cinco())
+
 #8.6)
+def seis ():
+    x=True
+    y=False
+    res = x and y
+    x = res and x
+    return x
+#print(seis())
 
 #----------------------------------------------------
-#9.1)
-#9.2)
-#9.3)
-#9.4)
+#9)
+def rt(x: int, g: int) -> int:
+    g = g +1
+    return x + g
 
+g:int = 0
+def ro(x: int) -> int:
+    global g
+    g = g + 1
+    return (x + g)
+#9.1)
+# 2  3  4 
+
+#9.2)
+# 2 2 2
