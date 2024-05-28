7d814da9d87d120a0a1c7af5339e8f26292a8d0c
Lucasbarmat <lucasbarmat@gmail.com>
2024-05-28 18:12:08 -0300

    g7

--- python/guia7.py
+++ python/guia7.py
@@ -21,7 +21,6 @@ def divide_a_todos(s:list, e:int)->bool:
             return False
     return True
 #print(divide_a_todos([3,0,6,2],2))
-
 def divide_a_todos_dos(s:list, e:int) ->bool:
     i = 0
     res:bool = True
@@ -41,10 +40,32 @@ def suma_total(s:list)->int:
 #print(suma_total([1,2,4,8]))
 
 #ej 1.4)
+def ordenanda(s:list)->bool:
+    for i in range(len(s) - 1):
+        if (s[i] >= s[i + 1]):
+            return False
+    return True
+#print(ordenanda([1, 2, 3, 1])) 
 
-    
 #ej 1.5)
+def mayor_a_7(s:list)->bool:
+    for i in range(len(s)):
+        if len(s[i]) == 7:
+            return True
+    return False
+#print(mayor_a_7(["fueeec","estageje","aaaaaaa"]))
+
 #ej 1.6)
+def palindromo(l:str)->bool:
+    res:bool = True
+    i:int = 0
+    while i < len(l):
+        if l[i] != l[len(l) - 1 - i]:
+            return False
+        i += 1
+    return res
+#print(palindromo("abvba"))
+
 #ej 1.7)
 def analizar_contra(contraseña:str)->str:
     if es_verde(contraseña):
@@ -55,8 +76,6 @@ def analizar_contra(contraseña:str)->str:
         else:
             return "amarilla"
         
-
-
 def es_roja(contraseña:str)->bool:
     if len(contraseña) < 5:
         return True
@@ -66,8 +85,6 @@ def es_verde(contraseña:str)->bool:
     if (longitud_mayor_ocho (contraseña)) and (una_minuscula (contraseña)) and (una_mayucula (contraseña)) and (un_digito (contraseña)):
         return True
     return False
-
-
 
 def longitud_mayor_ocho(contraseña:str)->bool:
     if len(contraseña) > 8:
@@ -91,10 +108,20 @@ def un_digito (contraseña:str)->bool:
         if '0' <= contraseña[letra] <='9':
             return True
     return False
-
 #print(analizar_contra("HolaG7holag"))
 
 #ej 1.8)
+def saldo(transacciones:float)->float:
+    saldo_actual:float = 0.0
+    for i in transacciones:
+        if i[0] == "I":
+            saldo_actual += i[1]
+        else: 
+            if i[0] == "R":
+                saldo_actual -=i[1]
+    return print(saldo_actual)
+#saldo([("I",2000), ("R", 20),("R", 1000),("I", 300)])
+
 #ej 1.9)
 #ej 1.10)
 
