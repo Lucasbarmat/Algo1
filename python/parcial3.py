a6183e972b16435b9ac6554cb6ca425c048a4c31
Lucasbarmat <lucasbarmat@gmail.com>
2024-06-08 19:08:45 -0300

    gh

--- python/parcial3.py
+++ python/parcial3.py
@@ -1,6 +1,5 @@
 """
 El próximo 19 de Noviembre se realizará en Argentina la segunda vuelta de las elecciones presidenciales. En esta competirán solo 2 listas (Lista UP; Lista LLA). En la mayor parte del país los salones de las escuelas ofician de cuartos oscuros. En ellos, las autoridades de mesa colocan las boletas sobre los pupitres. Dado que esta elección se realizará en una eṕoca donde muy probablemente haga mucho calor, no será raro el caso en el cual las boletas se vuelen y mezclen a causa de ventiladores prendidos a máxima potencia. Cuando esto ocurra, las autoridades deberán entrar al cuarto oscuro, juntar todas las boletas, acomodarlas por partido y volver a distribuirlas en sus lugares. Implementar una función acomodar que tome una lista con strings que representan el nombre de lista (UP o LLA) y devuelva una lista con la misma cantidad de elementos de cada uno de los posibles strings pero agrupadas, las de Lista UP al principio y las de lista LLA al final.
-
 No está permitido utilizar las funciones sort() y reverse().
 
 problema acomodar (in s: seq⟨String⟩) : seq⟨String⟩ {
@@ -14,11 +13,28 @@ Por ejemplo, dada
 s = ["LLA", "UP", "LLA", "LLA", "UP"]
 se debería devolver res = ["UP", "UP", "LLA", "LLA", "LLA"]
 """
+def acomodar(s:list[str])->list[str]:
+    res:list = []
+    for i in s:
+        if i == "UP":
+            res.append(i)
+    for i in s:
+        if i == "LLA":
+            res.append(i)
+    return res
+#print(acomodar(["LLA", "UP", "LLA","UP", "LLA", "UP"]))
+
 """
 2) Posición umbral [2 puntos]
-Durante una noche en un restaurant pasan varios grupos de diversa cantidad de personas. Para llevar control de esto, el dueño va anotando en su libreta cuánta gente entra y sale. Para hacerlo rápido decide que la mejor forma de llevarlo adelante es escribir un número al lado del otro, usando números positivos para los grupos que entran y negativos para los que salen. Gracias a estas anotaciones el dueño es capaz de hacer análisis del flujo de clientes. Por ejemplo, le interesa saber en qué momento de la noche superó una determinada cantidad de clientes totales que ingresaron (sin importar cuántos hay en el momento en el local).
+Durante una noche en un restaurant pasan varios grupos de diversa cantidad de personas. 
+Para llevar control de esto, el dueño va anotando en su libreta cuánta gente entra y sale. 
+Para hacerlo rápido decide que la mejor forma de llevarlo adelante es escribir un número al lado del otro, 
+usando números positivos para los grupos que entran y negativos para los que salen. Gracias a estas anotaciones el dueño es capaz de hacer análisis del flujo de clientes. 
+Por ejemplo, le interesa saber en qué momento de la noche superó una determinada cantidad de clientes totales que ingresaron 
+(sin importar cuántos hay en el momento en el local).
 
-Implementar la función pos_umbral, que dada una secuencia de enteros (puede haber negativos) devuelve la posición en la cual se supera el valor de umbral, teniendo en cuenta sólo los elementos positivos. Se debe devolver -1 si el umbral no se supera en ningún momento
+Implementar la función pos_umbral, que dada una secuencia de enteros (puede haber negativos) devuelve la posición en la cual se supera el valor de umbral, 
+teniendo en cuenta sólo los elementos positivos. Se debe devolver -1 si el umbral no se supera en ningún momento
 
 problema pos_umbral (in s: seq⟨Z⟩, in u: Z) : Z {
   requiere: u ≥ 0
@@ -29,10 +45,26 @@ s = [1,-2,0,5,-7,3]
 u = 5
 se debería devolver res = 3
 """
+def pos_umbral(s:list[int], u:int)->int:
+    res:int = -1
+    contador_de_personas = 0
+    i = 0
+    while i < len(s):
+        if s[i] > 0:
+            contador_de_personas += s[i]
+            if contador_de_personas > u:
+                return i
+            i +=1
+        i +=1
+    return res        
+#print(pos_umbral([5,-2,1,4,-7,0],5))
+        
 
 """
 3) Columnas repetidas [3 puntos]
-Implementar la función columnas_repetidas, que dada una matriz no vacía de m columnas (con m par y m ≥ 2) devuelve True si las primeras m/2 columnas son iguales que las últimas m/2 columnas. Definimos a una secuencia de secuencias como matriz si todos los elementos de la primera secuencia tienen la misma longitud.
+Implementar la función columnas_repetidas, que dada una matriz no vacía de m columnas (con m par y m ≥ 2) 
+devuelve True si las primeras m/2 columnas son iguales que las últimas m/2 columnas. Definimos a una secuencia de secuencias como matriz si 
+todos los elementos de la primera secuencia tienen la misma longitud.
 
 
 problema columnas_repetidas(in mat:seq⟨seq⟨Z⟩⟩ ) : Bool {
@@ -43,16 +75,42 @@ problema columnas_repetidas(in mat:seq⟨seq⟨Z⟩⟩ ) : Bool {
 }
 
 Por ejemplo, dada la matriz
-m = [[1,2,1,2],[-5,6,-5,6],[0,1,0,1]]
+m = [[1,  2,  3,  1,  2,  3],[-5, 6, -5,  6],[0,  1,  0,  1]]
 se debería devolver res = true
 TIP: para dividir un número entero x por 2 y obtener como resultado un número entero puede utilizarse la siguiente instrucción: int(x/2)
+
 """
+def primera_mitad(s:list[int])->list[int]:
+    res = []
+    for i in range(int(len(s)/2)):
+        res.append(s[i])
+    return res
+def segunda_mitad(s:list[int])->list[int]:
+    res = []
+    for i in range(int(len(s)/2),len(s)):
+        res.append(s[i])
+    return res
+def columnas_repetidas(mat:list[int])->bool:
+    for i in mat:
+        if not ((primera_mitad(i)) == segunda_mitad(i)):
+            return False
+    return True
+#print(columnas_repetidas([[1,  3, 1, 3],[0, 6, 0,  6],[0,  1,  0,  1]]))
+
+
 
 """
 4) Rugby 4 naciones [3 puntos]
-Desde hace más de 10 años existe en el mundo del rugby un torneo que disputan anualmente 4 selecciones del sur global (Argentina, Australia, Nueva Zelanda y Sudáfrica). Este torneo se llama "The rugby championship" o comunmente "4 naciones", ya que suplantó al viejo "3 naciones".
+Desde hace más de 10 años existe en el mundo del rugby un torneo que disputan anualmente 4 selecciones del sur global (Argentina, Australia, Nueva Zelanda y Sudáfrica). 
+Este torneo se llama "The rugby championship" o comunmente "4 naciones", ya que suplantó al viejo "3 naciones".
 
-Implementar la función cuenta_posiciones_por_nacion que dada la lista de naciones que compiten en el torneo, y el diccionario que tiene los resultados de los torneos anuales en el formato año:posiciones_naciones, donde año es un número entero y posiciones_naciones es una lista de strings con los nombres de las naciones, genere un diccionario de naciones:#posiciones, que para cada Nación devuelva la lista de cuántas veces salió en esa posición.
+Implementar la función cuenta_posiciones_por_nacion que dada la lista de naciones que compiten en el torneo, y el diccionario que tiene 
+los resultados de los torneos anuales 
+
+año : [posiciones_naciones]
+posiciones_naciones es una lista de strings con los nombres de las naciones
+
+genere un diccionario de naciones:#posiciones, que para cada Nación devuelva la lista de cuántas veces salió en esa posición.
 
 Tip: para crear una lista con tantos ceros como naciones se puede utilizar la siguiente sintaxis lista_ceros = [0]*len(naciones)
 
@@ -64,7 +122,21 @@ problema cuenta_posiciones_por_nacion(in naciones: seq⟨String⟩, in torneos: 
 }
 Por ejemplo, dados
 naciones= ["arg", "aus", "nz", "sud"]
-torneos= {2023:["nz", "sud", "arg", "aus"],
-                 2022:["nz", "sud", "aus", "arg"]}
+torneos= {2023:["nz", "sud", "arg", "aus"],2022:["nz", "sud", "aus", "arg"]}
 se debería devolver res = {"arg": [0,0,1,1], "aus": [0,0,1,1], "nz": [2,0,0,0], "sud": [0,2,0,0]}
 """
+
+def cuenta_posiciones_por_nacion(naciones:list[int], torneos:dict[int,list[str]])->dict[str,list[str]]:
+    res:dict = {}
+    for i in naciones:
+        if not i in res.keys():
+            lista_de_ceros = [0]*len(naciones)
+            res[i] = lista_de_ceros
+
+    for torneo in torneos:
+        for i in range(len(naciones)):
+            puesto_nacion = torneos[torneo][i]
+            res[puesto_nacion][i] = res[puesto_nacion][i] + 1
+    return res
+print(cuenta_posiciones_por_nacion(["arg", "aus", "nz", "sud"],{2023:["nz", "sud", "arg", "aus"],2022:["nz", "sud", "aus", "arg"]}))
+    
